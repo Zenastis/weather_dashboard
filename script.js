6598b2efe97ab20c8ebb5405ebc73028bbf0050d
samuel mcswain <samuelmcswain@hotmail.com>
2021-07-05 22:12:54 -0400

    fetch call for the 5 day working

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
 
 
 
+function forecast (searchValue){
+    if (!searchValue) {
+        return;
+    }
+    var forecastApi = `api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid={54e1375c02bb77b1a951b0a2a1264c0d}`
+    fetch(forecastApi)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+
+            var forecast2 = document.querySelector("#forecast");
+            forecast2.innerHTML = '<h4 class="mt-3">5day forecast</h4';
+            forecastElement = document.createElement('div');
+            forecastElement.className = '"row"';
+
+            for (var i = 0; i < data.list.length; i++) {
+                if (data.list[i].dt_txt.indexOf('15:00:00') !== -1) {
+                    var col = document.createElement('div');
+                    col.classList.add('col-md-2');
+                    var card = document.createElement('div');
+                    card.classList.add('card', 'bg-primary', 'text-white');
+                    var wind = document.createElement('p');
+                    wind.classList.add('card-text');
+                    wind.textContent= `Wind Speed: ${data.list[i].wind.speed} mph`;
+                    var humidity = document.createElement('p');
+                    humidity.classList.add('card-text');
+                    humidity.textContent = `Humidity: ${data.list[i].main.humidity} %`
+                    var body = document.createElement('div');
+                    body.classList.add('card-body', 'p-2');
+                    var title = document.createElement('h5');
+                    title.classList.add('card-title');
+                    title.textContent = new Date(
+                        data.list[i].dt_txt
+                    ).toLocaleDateString();
+                    var img = document.createElement('img');
+                    img.setAttribute(
+                        'src',
+                        `http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`
+                    );
+                    var para1 = document.createElement('p');
+                    para1.classList.add('card-text');
+                    para1.textContent = `Temp: ${data.list[i].main.temp_max} degrees`;
+                    var para2 = document.createElement('p');
+                    para2.classList.add('card-text');
+                    para2.textContent = `Humidity: ${data.list[i].main.humidity} %`;
+                }
+            }
+        })
+}
 
 function weather (searchValue){
 
@@ -19,20 +68,6 @@ function weather (searchValue){
 }
 weather()
 
-function forecast (searchValue){
-    if (!searchValue) {
-        return;
-    }
-    var forecastApi = `api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid={54e1375c02bb77b1a951b0a2a1264c0d}`
-    fetch(forecastApi)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data)
-
-            var forecast2 = document.querySelector("#forecast");
-            forecast2.innerHTML = <h4 class="mt"
-        })
-}
 
 function oneCall (){
     var requestOneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=35.2271&lon=-80.8431&exclude=hourly&appid=54e1375c02bb77b1a951b0a2a1264c0d"
